5ce7ac1d87ea4fdf0a20d9957492a60e338ccae1
igreukrnet <igre@ukr.net>
2023-11-22 12:35:34 +0200

    first commit

---
+++ src/test/java/seletest.java
@@ -0,0 +1,25 @@
+//import static com.codeborne.selenide.Condition.disappear;
+//import static com.codeborne.selenide.Condition.text;
+//import static com.codeborne.selenide.Selenide.*;
+//import org.openqa.selenium.By;
+import io.qameta.allure.Issue;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Slf4j
+public class seletest {
+    @Test
+    @Issue(value="IOP-289345")
+    public void userCanLoginByUsername() {
+        step("AAAAA");
+        log.error("self log");
+        System.out.println("sdvgsdg");
+//        open("/login");
+//        $(By.name("user.name")).setValue("johny");
+//        $("#submit").click();
+//        $(".loading_progress").should(disappear); // Waits until element disappears
+//        $("#username").shouldHave(text("Hello, Johny!")); // Waits until element gets text
+    }
+}
